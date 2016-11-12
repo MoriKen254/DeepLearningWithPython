427c86e810bf52e6e5e3ffd3bff143c8246a76c5
Masaru Morita <masamorobo@gmail.com>
2016-11-13 08:27:17 +0900

    [add] license description to codes.

--- SingleLayerNeuralNetworks/Perceptrons.py
+++ SingleLayerNeuralNetworks/Perceptrons.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+u"""
+Copyright (c) 2016 Masaru Morita
+
+This software is released under the MIT License.
+See LICENSE file included in this repository.
+"""
+
 import sys
 import random
 
