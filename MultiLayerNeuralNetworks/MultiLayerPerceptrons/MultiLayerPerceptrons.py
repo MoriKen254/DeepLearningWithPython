1c5ddc6803853e48eb77bd337fedbaabc56a0102
Masaru Morita <masamorobo@gmail.com>
2016-11-16 08:06:16 +0900

    Add empty MultiLayerPercetrsons file.

---
+++ MultiLayerNeuralNetworks/MultiLayerPerceptrons/MultiLayerPerceptrons.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+u"""
+Copyright (c) 2016 Masaru Morita
+
+This software is released under the MIT License.
+See LICENSE file included in this repository.
+"""
+
